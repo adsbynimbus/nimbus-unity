01f26b222b71757ebba1d86a71e68cc31d0f00af
Jason Sznol <jason@timehop.com>
2021-05-03 13:50:13 -0400

    Build now works with 2018

--- sample-app/Packages/AdsByNimbus/Nimbus/Editor/AndroidPostBuildProcessor.cs
+++ sample-app/Packages/AdsByNimbus/Nimbus/Editor/AndroidPostBuildProcessor.cs
@@ -11,11 +11,44 @@ namespace Nimbus.Editor {
 
 		public void OnPostGenerateGradleAndroidProject(string path) {
 			Debug.Log("Bulid path : " + path);
-			StreamWriter writer = File.AppendText(path + "/gradle.properties");
-			writer.WriteLine("android.useAndroidX=true");
-			writer.WriteLine("android.enableJetifier=true");
-			writer.Flush();
-			writer.Close();
+			StreamWriter propWriter = File.AppendText(path + "/gradle.properties");
+			propWriter.WriteLine(@"
+				android.useAndroidX=true
+				android.enableJetifier=true");
+			propWriter.Flush();
+			propWriter.Close();
+
+			#if !UNITY_2020_1_OR_NEWER
+			StreamWriter buildWriter = File.AppendText(path + "/build.gradle");
+			buildWriter.WriteLine(@"
+				allprojects {
+					repositories {
+						maven {
+							url = uri(""https://adsbynimbus-public.s3.amazonaws.com/android/sdks"")
+							credentials {
+								username = ""*""
+							}
+						}
+					}
+				}");
+			buildWriter.WriteLine(@"
+				dependencies {
+					implementation ""com.adsbynimbus.android:nimbus:1.10.0""
+					implementation ""com.adsbynimbus.android:extension-okhttp:1.10.0""
+					implementation ""org.jetbrains.kotlin:kotlin-reflect:1.4.31""
+				}
+			");
+			buildWriter.Flush();
+			buildWriter.Close();
+
+			StreamWriter proguardWriter = File.AppendText(path + "/proguard-unity.txt");
+			proguardWriter.WriteLine(@"
+			-keep class com.nimbus.demo.UnityHelper { *; }
+			-keep class com.adsbynimbus.** { *; }
+			");
+			proguardWriter.Flush();
+			proguardWriter.Close();
+			#endif
 		}
 	}
 }
