7c200c25cc40430a6faa4ad0e099262ecce08a85
Jason Sznol <jason@timehop.com>
2021-05-03 14:22:46 -0400

    Added support for building on Unity 2020+ Cleanup of the output to the gradle file

--- sample-app/Packages/AdsByNimbus/Nimbus/Editor/AndroidPostBuildProcessor.cs
+++ sample-app/Packages/AdsByNimbus/Nimbus/Editor/AndroidPostBuildProcessor.cs
@@ -9,46 +9,48 @@ namespace Nimbus.Editor {
 			get { return 999; }
 		}
 
-		public void OnPostGenerateGradleAndroidProject(string path) {
-			Debug.Log("Bulid path : " + path);
-			StreamWriter propWriter = File.AppendText(path + "/gradle.properties");
-			propWriter.WriteLine(@"
-				android.useAndroidX=true
-				android.enableJetifier=true");
-			propWriter.Flush();
-			propWriter.Close();
-
+		public void OnPostGenerateGradleAndroidProject(string path) {			
 			#if !UNITY_2020_1_OR_NEWER
 			StreamWriter buildWriter = File.AppendText(path + "/build.gradle");
 			buildWriter.WriteLine(@"
-				allprojects {
-					repositories {
-						maven {
-							url = uri(""https://adsbynimbus-public.s3.amazonaws.com/android/sdks"")
-							credentials {
-								username = ""*""
-							}
-						}
-					}
-				}");
+allprojects {
+	repositories {
+		maven {
+			url = uri(""https://adsbynimbus-public.s3.amazonaws.com/android/sdks"")
+			credentials {
+				username = ""*""
+			}
+		}
+	}
+}");
 			buildWriter.WriteLine(@"
-				dependencies {
-					implementation ""com.adsbynimbus.android:nimbus:1.10.0""
-					implementation ""com.adsbynimbus.android:extension-okhttp:1.10.0""
-					implementation ""org.jetbrains.kotlin:kotlin-reflect:1.4.31""
-				}
-			");
+dependencies {
+	implementation ""com.adsbynimbus.android:nimbus:1.10.0""
+	implementation ""com.adsbynimbus.android:extension-okhttp:1.10.0""
+	implementation ""org.jetbrains.kotlin:kotlin-reflect:1.4.31""
+}");
 			buildWriter.Flush();
 			buildWriter.Close();
 
 			StreamWriter proguardWriter = File.AppendText(path + "/proguard-unity.txt");
 			proguardWriter.WriteLine(@"
-			-keep class com.nimbus.demo.UnityHelper { *; }
-			-keep class com.adsbynimbus.** { *; }
-			");
+-keep class com.nimbus.demo.UnityHelper { *; }
+-keep class com.adsbynimbus.** { *; }");
 			proguardWriter.Flush();
 			proguardWriter.Close();
-			#endif
+			writeGradleProps(path + "/gradle.properties")
+			#else
+			writeGradleProps(path + "/../gradle.properties")
+			#endif				
+		}
+
+		private void writeGradleProps(String path) {
+			StreamWriter propWriter = File.AppendText();
+			propWriter.WriteLine(@"
+android.useAndroidX=true
+android.enableJetifier=true");
+			propWriter.Flush();
+			propWriter.Close();
 		}
 	}
 }
