17064290e0d9aef97ea943ffc1ee70983829b078
Marc Santiago <marcanthonysanti@gmail.com>
2021-05-03 12:53:21 -0400

    Editor: Adding Editor assembly

---
+++ sample-app/Packages/AdsByNimbus/Nimbus/Editor/AndroidPostBuildProcessor.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor.Android;
+using UnityEngine;
+
+namespace Nimbus.Editor {
+	public class AndroidPostBuildProcessor : IPostGenerateGradleAndroidProject {
+		public int callbackOrder {
+			get { return 999; }
+		}
+
+		public void OnPostGenerateGradleAndroidProject(string path) {
+			Debug.Log("Bulid path : " + path);
+			StreamWriter writer = File.AppendText(path + "/gradle.properties");
+			writer.WriteLine("android.useAndroidX=true");
+			writer.WriteLine("android.enableJetifier=true");
+			writer.Flush();
+			writer.Close();
+		}
+	}
+}
+#endif
