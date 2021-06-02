9b95ab59fa52e984ed6b555f8623b9f451429f02
Bruno Bruggemann <brunob@timehop.com>
2021-06-02 16:02:07 -0300

    Changed IOSAdManager to be an internal class

--- package/Runtime/Scripts/Internal/IOSAdManager.cs
+++ package/Runtime/Scripts/Internal/IOSAdManager.cs
@@ -2,13 +2,13 @@
 
 namespace Nimbus.Runtime.Scripts.Internal
 {
-    public class IOSAdManager : MonoBehaviour
+    internal class IOSAdManager : MonoBehaviour
     {
         private NimbusAdUnit adUnit;
 
         private static IOSAdManager instance;
 
-        public static IOSAdManager Instance
+        internal static IOSAdManager Instance
         {
             get
             {
@@ -32,27 +32,27 @@ namespace Nimbus.Runtime.Scripts.Internal
             DontDestroyOnLoad(gameObject);
         }
 
-        public void SetAdUnit(NimbusAdUnit adUnit)
+        internal void SetAdUnit(NimbusAdUnit adUnit)
         {
             this.adUnit = adUnit;
         }
 
         #region iOS Event Callbacks
 
-        public void OnAdRendered(string param)
+        internal void OnAdRendered(string param)
         {
             Debug.unityLogger.Log("OnAdRendered");
             adUnit.AdWasRendered = true;
             adUnit.EmitOnAdRendered(adUnit);
         }
 
-        public void OnError(string param)
+        internal void OnError(string param)
         {
             Debug.unityLogger.Log("OnError: " + param);
             adUnit.EmitOnAdError(adUnit);
         }
 
-        public void OnAdEvent(string param)
+        internal void OnAdEvent(string param)
         {
             Debug.unityLogger.Log("OnAdEvent");
             AdEventTypes eventType = (AdEventTypes)System.Enum.Parse(typeof(AdEventTypes), param, true);
