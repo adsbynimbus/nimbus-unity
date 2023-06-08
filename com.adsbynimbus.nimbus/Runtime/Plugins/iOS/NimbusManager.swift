71d0796f29cd9133dbb3b6bcceeb4d083d19f4da
Jason Sznol <jason@timehop.com>
2023-06-08 15:00:14 -0400

    Fixed issue with iOS 13 and Swift

--- com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusManager.swift
+++ com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusManager.swift
@@ -77,12 +77,7 @@ import NimbusRequestAPSKit
     }
     
     private func unityViewController() -> UIViewController? {
-        UIApplication.shared.connectedScenes
-            .filter { $0.activationState == .foregroundActive }
-            .compactMap { $0 as? UIWindowScene }
-            .first?.windows
-            .filter { $0.isKeyWindow }
-            .first?.rootViewController
+        UnityFramework.getInstance().appController().rootViewController
     }
     
     // MARK: - Public Functions
