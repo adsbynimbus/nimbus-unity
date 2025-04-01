c47bb56d0c925c17452032ffe2736f1b39f1a3b1
Jonathan Sligh <jonathan@nimbus.co>
2025-04-01 09:53:06 -0500

    More iOS changes

--- com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/Interceptor/ThirdPartyDemand/MobileFuse/MobileFuseIOS.cs
+++ com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/Interceptor/ThirdPartyDemand/MobileFuse/MobileFuseIOS.cs
@@ -2,21 +2,21 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Nimbus.Internal.Utility;
 using OpenRTB.Request;
 using UnityEngine;
 
 [assembly: InternalsVisibleTo("nimbus.test")]
 namespace Nimbus.Internal.Interceptor.ThirdPartyDemand.UnityAds {
-	#if UNITY_IOS && NIMBUS_ENABLE_UNITY_ADS
-	internal class MoileFuseIOS : IInterceptor, IProvider {
-		private readonly string _gameID;
+	#if UNITY_IOS && NIMBUS_ENABLE_MOBILEFUSE
+	internal class MobileFuseIOS : IInterceptor, IProvider {
 		
 		[DllImport("__Internal")]
-		private static extern void _initializeUnityAds(string gameId);
+		private static extern void _initializeMobileFuse();
         
 		[DllImport("__Internal")]
-		private static extern string _fetchUnityAdsToken();
+		private static extern string _fetchMobileFuseToken();
 
 		public BidRequest ModifyRequest(BidRequest bidRequest, string data) {
 			if (data.IsNullOrEmpty()) {
@@ -25,23 +25,20 @@ namespace Nimbus.Internal.Interceptor.ThirdPartyDemand.UnityAds {
 			if (bidRequest.User.Ext == null) {
 				bidRequest.User.Ext = new UserExt();
 			}
-			bidRequest.User.Ext.UnityBuyerId = data;
+			var mobileFuseObject = JsonConvert.DeserializeObject(data, typeof(JObject)) as JObject;
+			bidRequest.User.Ext.MobileFuseBuyerData = mobileFuseObject;
 			return bidRequest;
 		}
 
 		public string GetProviderRtbDataFromNativeSDK(AdUnitType type, bool isFullScreen)
 		{
-			var biddingToken = _fetchUnityAdsToken();
-			Debug.unityLogger.Log("Unity Token", biddingToken);
+			var biddingToken = _fetchMobileFuseToken();
+			Debug.unityLogger.Log("MobileFuse Token", biddingToken);
 			return biddingToken;
 		}
 
-		public UnityAdsIOS(string gameID) {
-			_gameID = gameID;
-		}
-
 		public void InitializeNativeSDK() {
-			_initializeUnityAds(_gameID);
+			_initializeMobileFuse();
 		}
 
 	}
