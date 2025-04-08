66fb90c7b89191d993a9d67d120c020aac32a6b6
Jonathan Sligh <jonathan@nimbus.co>
2025-04-08 14:22:37 -0500

    wrong string

--- com.adsbynimbus.nimbus/VersionConstants.cs
+++ com.adsbynimbus.nimbus/VersionConstants.cs
@@ -3,7 +3,7 @@ namespace Nimbus
 {
     public static class VersionConstants
     {
-        public const string UnitySdkVersion = "1.11";
+        public const string UnitySdkVersion = "1.11.0";
         public const string IosSdkVersion = "2.27.1";
         public const string AndroidSdkVersion = "2.28.2";
     }
