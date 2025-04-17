d9741a8cdc23bb73a6dc23e2987c8b08ac799e2e
Jonathan Sligh <jonathan@nimbus.co>
2025-04-17 11:13:28 -0500

    Revert "Fixing unnecessary changes."
    This reverts commit 9837574f57d243db064702803f11a6097ee6f2cb.

--- com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/LiveRamp/NimbusLiveRampHelpers.cs
+++ com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/LiveRamp/NimbusLiveRampHelpers.cs
@@ -10,68 +10,68 @@ using UnityEngine;
 namespace Nimbus.Internal.LiveRamp
 {
     #if NIMBUS_ENABLE_LIVERAMP
-        public class NimbusLiveRampHelpers
+    public class NimbusLiveRampHelpers
+    {
+        #if UNITY_IOS 
+            [DllImport("__Internal")]
+            private static extern void _initializeLiveRamp(String configId, String email, 
+                String phoneNumber, Boolean isTestMode, Boolean hasConsentForNoLegislation);
+
+            [DllImport("__Internal")]
+            private static extern string _getLiveRampData();
+        #endif
+
+        public static void initializeLiveRamp(String configId, 
+            Boolean hasConsentForNoLegislation, Boolean isTestMode, String email = "", 
+            String phoneNumber = "")
         {
             #if UNITY_IOS
-                [DllImport("__Internal")]
-                private static extern void _initializeLiveRamp(String configId, String email, 
-                    String phoneNumber, Boolean isTestMode, Boolean hasConsentForNoLegislation);
-
-                [DllImport("__Internal")]
-                private static extern string _getLiveRampData();
+                _initializeLiveRamp(configId, email, phoneNumber, isTestMode, hasConsentForNoLegislation);
             #endif
-
-            public static void initializeLiveRamp(String configId, 
-                Boolean hasConsentForNoLegislation, Boolean isTestMode, String email = "", 
-                String phoneNumber = "")
-            {
-                #if UNITY_IOS
-                    _initializeLiveRamp(configId, email, phoneNumber, isTestMode, hasConsentForNoLegislation);
-                #endif
-                #if UNITY_ANDROID
-                    var liveRamp = new AndroidJavaClass("com.adsbynimbus.request.LiveRampExtension");
-                    if (isTestMode)
-                    {
-                        liveRamp.CallStatic("initializeTestMode", configId, email);
-                    }
-                    else
-                    {
-                        liveRamp.CallStatic("initialize", configId, email, phoneNumber, hasConsentForNoLegislation);
-                    }
-                #endif
-            }
-
-            public static BidRequest addLiveRampToRequest(BidRequest bidRequest)
-            {
-                var liveRampData = "";
-                Eid[] eidsObject = {};
-                #if UNITY_IOS
-                    liveRampData = _getLiveRampData();
-                #endif
-                #if UNITY_ANDROID
-                    try 
-                    {
-                        liveRampData = BridgeHelpers.GetStringFromJavaFuture(
-                            "com.adsbynimbus.request.internal.NimbusRequestLiverampInternal",
-                            "fetchLiverampEnvelope", new object[]{},500L);
-                    }
-                    catch (Exception e)
-                    {
-                        Debug.unityLogger.Log("LiveRamp Request Info ERROR", e.Message);
-                    }
-                #endif
-                if (liveRampData.IsNullOrEmpty())
+            #if UNITY_ANDROID
+                var liveRamp = new AndroidJavaClass("com.adsbynimbus.request.LiveRampExtension");
+                if (isTestMode)
                 {
-                    return bidRequest;
+                    liveRamp.CallStatic("initializeTestMode", configId, email);
                 }
-                eidsObject = JsonConvert.DeserializeObject(liveRampData, typeof(Eid[])) as Eid[];
-                bidRequest.User ??= new User();
-                bidRequest.User.Ext ??= new UserExt();
-                bidRequest.User.Ext.Eids = eidsObject;
+                else
+                {
+                    liveRamp.CallStatic("initialize", configId, email, phoneNumber, hasConsentForNoLegislation);
+                }
+            #endif
+        }
+
+        public static BidRequest addLiveRampToRequest(BidRequest bidRequest)
+        {
+            var liveRampData = "";
+            Eid[] eidsObject = {};
+            #if UNITY_IOS
+                liveRampData = _getLiveRampData();
+            #endif
+            #if UNITY_ANDROID
+                try 
+                {
+                    liveRampData = BridgeHelpers.GetStringFromJavaFuture(
+                        "com.adsbynimbus.request.internal.NimbusRequestLiverampInternal",
+                        "fetchLiverampEnvelope", new object[]{},500L);
+                }
+                catch (Exception e)
+                {
+                    Debug.unityLogger.Log("LiveRamp Request Info ERROR", e.Message);
+                }
+            #endif
+            if (liveRampData.IsNullOrEmpty())
+            {
                 return bidRequest;
             }
+            eidsObject = JsonConvert.DeserializeObject(liveRampData, typeof(Eid[])) as Eid[];
+            bidRequest.User ??= new User();
+            bidRequest.User.Ext ??= new UserExt();
+            bidRequest.User.Ext.Eids = eidsObject;
+            return bidRequest;
         }
-    #endif
+    }
+#endif
 }
         
         
