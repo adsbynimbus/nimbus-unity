96332b0a3a7dddf27a3c86dbb23eafdb596de4b5
Jonathan Sligh <jonathan@nimbus.co>
2025-05-14 11:25:04 -0500

    Version number updates for iOS and Android

--- com.adsbynimbus.nimbus/VersionConstants.cs
+++ com.adsbynimbus.nimbus/VersionConstants.cs
@@ -3,8 +3,8 @@ namespace Nimbus
 {
     public static class VersionConstants
     {
-        public const string UnitySdkVersion = "1.12.0";
-        public const string IosSdkVersion = "2.27.2";
-        public const string AndroidSdkVersion = "2.29.0";
+        public const string UnitySdkVersion = "1.12.1";
+        public const string IosSdkVersion = "2.28.1";
+        public const string AndroidSdkVersion = "2.30.0";
     }
 }
