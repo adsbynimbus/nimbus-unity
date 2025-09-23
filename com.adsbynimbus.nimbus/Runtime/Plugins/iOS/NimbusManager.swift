3afe7a04558ee5c7acd9595a30c2067f0f91fa07
Jonathan Sligh <jonathan@nimbus.co>
2025-09-23 11:01:07 -0500

    SafeArea fix from their side worked.

--- com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusManager.swift
+++ com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusManager.swift
@@ -426,7 +426,7 @@ import InMobiSDK
                     contentView.centerXAnchor.constraint(equalTo: viewController.view.centerXAnchor),
                     contentView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor),
                     contentView.trailingAnchor.constraint(equalTo: viewController.view.trailingAnchor),
-                    contentView.bottomAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.bottomAnchor)
+                    contentView.bottomAnchor.constraint(equalTo: viewController.view.bottomAnchor)
                 ])
                             
             adController = Nimbus.load(ad: nimbusAd, container: contentView, adPresentingViewController: viewController, delegate: self)
