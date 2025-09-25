c7fe822c6c369312d89bc06c1cf590aac32e1225
Jonathan Sligh <jonathan@nimbus.co>
2025-09-25 12:40:15 -0500

    Added in AdUnit support.

--- com.adsbynimbus.nimbus/Runtime/Plugins/RTB/Request/Imp.cs
+++ com.adsbynimbus.nimbus/Runtime/Plugins/RTB/Request/Imp.cs
@@ -37,5 +37,37 @@ namespace OpenRTB.Request {
 
         [JsonProperty("facebook_test_ad_type", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string MetaTestAdType { get; set; }
+        
+        [JsonProperty("adunit", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public ImpExtAdUnitType AdUnitType { get; set; }
+    }
+
+    public enum ImpExtAdUnitType
+    {
+        // AdUnitTypeUnknown represents an unknown ad unit type.
+        Unknown = 0,
+
+        // AdUnitTypeInline represents banner ads typically sized at or below 300x250.
+        // Common dimensions include 300x250, 300x50, 320x50, 320x100, and 728x90.
+        // It May also include video ads when the video.placementType is set to "inline".
+        Inline = 1,
+
+        // AdUnitTypeInterstitial represents full-screen, blocking ads, typically sized at 320x480.
+        Interstitial = 2,
+
+        // AdUnitTypeRewarded represents ads that grant a reward to users upon completion.
+        // While often video-based, the format is not limited to video.
+        // For video ads, users are typically required to watch to completion; behavior for other formats is still to be defined.
+        Rewarded = 3,
+
+        // AdUnitTypeNative represents native ads, where the ad UI is constructed client-side by the publisher.
+        // These ads are typically non-blocking and styled to match the app’s content.
+        // Currently used as a placeholder, as official support may vary.
+        Native = 4,
+	
+        // AdUnitTypeNimbusDynamic represents Nimbus’s proprietary dynamic ad unit.
+        // It simultaneously requests RTB Banner, RTB Video, and occasionally RTB Native creatives.
+        // This unit is typically displayed inline, which distinguishes it from interstitial units.
+        NimbusDynamic = 5
     }
 }
