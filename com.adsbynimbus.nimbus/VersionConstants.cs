ac70dab0919f8ef9792e385ff7b5c04f374545b5
jonathansligh-nimbus <jonathan@nimbus.co>
2025-10-30 10:49:22 -0500

    Updated version number for release. (#120)

--- com.adsbynimbus.nimbus/VersionConstants.cs
+++ com.adsbynimbus.nimbus/VersionConstants.cs
@@ -3,7 +3,7 @@ namespace Nimbus
 {
     public static class VersionConstants
     {
-        public const string UnitySdkVersion = "1.16.0";
+        public const string UnitySdkVersion = "1.17.0";
         public const string IosSdkVersion = "2.31.1";
         public const string AndroidSdkVersion = "2.35.0";
     }
