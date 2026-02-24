ea323b392ef1651d61b55ebbaebc41328db4c2fd
Jonathan Sligh <jonathan@nimbus.co>
2026-02-24 15:36:13 -0600

    Fixed compiler error caught by automatic build system.

--- sample-app/Assets/Example/Scripts/GUIButtonTest.cs
+++ sample-app/Assets/Example/Scripts/GUIButtonTest.cs
@@ -60,7 +60,7 @@ namespace Example.Scripts {
 
 		public void OnAdError(NimbusAdUnit nimbusAdUnit) {
 			Debug.unityLogger.Log(
-				$"Ad unit of {nimbusAdUnit.InstanceID} type {nimbusAdUnit.AdType} could not be rendered {nimbusAdUnit.ErrResponse.Message}");
+				$"Ad unit of {nimbusAdUnit.InstanceID} type {nimbusAdUnit.AdType} could not be rendered {nimbusAdUnit.ErrResponse}");
 		}
 
 		public void LoadAndShowBanner() {
