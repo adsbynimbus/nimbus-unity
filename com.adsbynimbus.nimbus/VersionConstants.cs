5b54784a508eead04aeacd5672780780e598adb9
Jonathan Sligh <jonathan@nimbus.co>
2026-03-19 13:46:37 -0500

    Updated iOS SDK to 2.32.4 and Android SDK to 2.35.1

--- com.adsbynimbus.nimbus/VersionConstants.cs
+++ com.adsbynimbus.nimbus/VersionConstants.cs
@@ -4,7 +4,7 @@ namespace Nimbus
     public static class VersionConstants
     {
         public const string UnitySdkVersion = "1.17.2";
-        public const string IosSdkVersion = "2.31.1";
-        public const string AndroidSdkVersion = "2.35.0";
+        public const string IosSdkVersion = "2.32.4";
+        public const string AndroidSdkVersion = "2.35.1";
     }
 }
