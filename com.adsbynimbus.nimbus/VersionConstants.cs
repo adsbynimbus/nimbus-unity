34889c9962f362907cf0bb83c81bd76c8f2bfc3e
Jonathan Sligh <jonathan@nimbus.co>
2026-05-26 08:45:03 -0500

    Changed version numbers.

--- com.adsbynimbus.nimbus/VersionConstants.cs
+++ com.adsbynimbus.nimbus/VersionConstants.cs
@@ -5,6 +5,6 @@ namespace Nimbus
     {
         public const string UnitySdkVersion = "3.0.0";
         public const string IosSdkVersion = "3.0.0-rc.1";
-        public const string AndroidSdkVersion = "2.36.1";
+        public const string AndroidSdkVersion = "3.0.0-rc.1";
     }
 }
