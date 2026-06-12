5098c8cae5498d80dafabb9010125647616b8345
Jonathan Sligh <jonathan@nimbus.co>
2026-06-12 12:36:40 -0500

    Fix build

--- com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/Extensions/BridgeHelpers.cs
+++ com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/Extensions/BridgeHelpers.cs
@@ -77,7 +77,7 @@ namespace Nimbus.Internal.Extensions
         public String appId;
     }
 #endif
-#if UNITY_ANDROID
+#if UNITY_ANDROID || UNITY_EDITOR
     public class Extensions
     {
         public String apsAppKey;
