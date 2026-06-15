fbd74d30894cae072eed761c49857361e5f7731a
Jonathan Sligh <jonathan@nimbus.co>
2026-06-15 12:29:29 -0500

    Adding base config helpers

---
+++ com.adsbynimbus.nimbus/Runtime/Scripts/Nimbus.Internal/Extensions/ConfigHelpers.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+
+namespace Nimbus.Internal.Extensions
+{
+    public class ConfigHelpers
+    {
+        public static void setSessionId(string sessionId)
+        {
+            
+        }
+        
+        public static void setCoppa(bool coppa)
+        {
+            
+        }
+
+        //universal app-wide
+        public static void setApp()
+        {
+            
+        }
+
+        //universal app-wide
+        public static void setUser()
+        {
+            
+        }
+
+        public static void setBlockedAdvertisingDomains(string[] blockedAdvertisingDomains)
+        {
+            
+        }
+
+        public static void setRequestUrl(string requestUrl)
+        {
+            
+        }
+
+        public static void setAdditionalRequestHeaders(Dictionary<string, string> additionalRequestHeaders)
+        {
+            
+        }
+
+        public static void setInterceptorTimeout(int interceptorTimeout)
+        {
+            
+        }
+        
+        public static void showMuteButton(bool showMuteButton)
+        {
+        }
+        
+        //very difficult, maybe say the can add it in swift/kotlin if needed?
+        public static void setVerificationProviders()
+        {
+            
+        }
+
+        public static void enableSwipeProtection(bool enableSwipeProtection)
+        {
+            
+        }
+        
+        //very difficult, maybe say the can add it in swift/kotlin if needed?
+        public static void setIdentityProvider()
+        {
+            
+        }
+
+        public static void setIsSKOverlayEnabledForAllUnits()
+        {
+            
+        }
+
+        public static void setIABProperties()
+        {
+            
+        }
+    }
+    //TODO: User, app, iab objects
+}
