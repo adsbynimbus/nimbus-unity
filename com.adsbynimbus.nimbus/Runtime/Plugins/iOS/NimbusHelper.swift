15d623bbbf8135db65e2792eed14701c15b15235
Jonathan Sligh <jonathan@nimbus.co>
2026-07-21 10:02:07 -0500

    Moved structs to helper.

--- com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusHelper.swift
+++ com.adsbynimbus.nimbus/Runtime/Plugins/iOS/NimbusHelper.swift
@@ -2,7 +2,8 @@
 //  NimbusHelper.swift
 //  UnityFramework
 //
-//  Created by Victor Takai on 10/06/22.
+//  Created by Jonathan Sligh on 05/07/26.
+//  Copyright © 2026 AdsByNimbus. All rights reserved.
 //
 
 import Foundation
@@ -198,6 +199,41 @@ import AppTrackingTransparency
         let vendorKey: String
         let parameters: String
     }
+    
+    
+    public static func extensionsFromJsonString(thirdPartyDemand: String) -> Extensions? {
+        var extensions: Extensions?
+        if (thirdPartyDemand != "" && !thirdPartyDemand.isEmpty) {
+            do {
+                if let dataFromString = thirdPartyDemand.data(using: .utf8) {
+                    extensions = try JSONDecoder().decode(Extensions.self, from: dataFromString)
+                }
+            } catch {
+                NimbusHelper.didReceiveNimbusError(
+                    adUnitInstanceID: -1,
+                    error: .unitysdk(stage: .request, detail: "Failed to decode third party json: \(error)")
+                )
+            }
+        }
+        return extensions
+    }
+    
+    public static func requestModifiersFromJsonString(requestModifiers: String) -> RequestModifiers? {
+        var modifiers: RequestModifiers?
+        if (requestModifiers != "" && !requestModifiers.isEmpty) {
+            do {
+                if let dataFromString = requestModifiers.data(using: .utf8) {
+                    modifiers = try JSONDecoder().decode(RequestModifiers.self, from: dataFromString)
+                }
+            } catch {
+                NimbusHelper.didReceiveNimbusError(
+                    adUnitInstanceID: -1,
+                    error: .unitysdk(stage: .request, detail: "Failed to decode request modifiers json: \(error)")
+                )
+            }
+        }
+        return modifiers
+    }
 }
 
 extension NimbusError.Domain {
@@ -209,3 +245,266 @@ extension NimbusError {
         NimbusError(reason: reason, domain: .unitysdk, stage: stage, detail: detail)
     }
 }
+
+
+public struct Extensions: Codable {
+    let aps: Aps?
+    let adMob: AdMob?
+    let inMobi: InMobi?
+    let meta: Meta?
+    let mintegral: Mintegral?
+    let mobileFuse: MobileFuse?
+    let moloco: Moloco?
+    let unityAds: UnityAds?
+    let vungle: Vungle?
+}
+
+
+public struct RequestModifiers: Codable, Sendable {
+    let app: PerRequestApp?
+    let banner: BannerCreative?
+    let environment: Env?
+    let location: Location?
+    let userKeywords: String?
+    let video: VideoCreative?
+    let viewability: Viewability?
+    
+    @MainActor
+    var components: [RequestComponent] {
+        var requestComponents: [RequestComponent] = []
+        if let perRequestApp = app {
+            requestComponents.append(perRequestApp.requestComponent)
+        }
+        if let bannerCreative = banner {
+            requestComponents.append(bannerCreative.requestComponent)
+        }
+        if let env = environment {
+            requestComponents.append(env.requestComponent)
+        }
+        if let loc = location {
+            requestComponents.append(loc.requestComponent)
+        }
+        if let userKeywords = userKeywords {
+            requestComponents.append(user(keywords: userKeywords))
+        }
+        if let vid = video {
+            requestComponents.append(vid.requestComponent)
+        }
+        if let v = viewability {
+            requestComponents.append(v.requestComponent)
+        }
+        return requestComponents
+    }
+}
+
+public struct BannerCreative: Codable, UnityRequestComponent, Sendable {
+    let width: Int?
+    let height: Int?
+    let bidFloor: Float?
+    
+    let rawAddFormats: [Int]?
+    private let rawAdPosition: Int?
+    private let rawBattr: [Int]?
+    
+    enum CodingKeys: String, CodingKey {
+        case width, height, bidFloor
+        case rawAddFormats = "addFormats"
+        case rawAdPosition = "adPosition"
+        case rawBattr = "battr"
+    }
+    
+    //this is needed because RTB.Position's declaration is overriding swift's normal decode methods
+    var adPosition: RTB.Position? {
+        if let position = rawAdPosition {
+            return RTB.Position(rawValue: position)
+        }
+        return nil
+    }
+    
+    //this is needed because RTB.CreativeAttribute's declaration is overriding swift's normal decode methods
+    var battr: Set<RTB.CreativeAttribute>? {
+        guard let rawBattr = rawBattr else { return nil }
+        return Set(rawBattr.compactMap { intValue in
+            RTB.CreativeAttribute(rawValue: intValue)
+        })
+    }
+    
+    var requestComponent: any RequestComponent {
+        var adSize: AdSize? = nil
+        if let width = width, let height = height {
+            adSize = AdSize(width: width, height: height)
+        }
+        var addFormats: Set<RTB.Format> = []
+        //this has to be done here because RTB.Format.Interstitial is @MainActor locked
+        if let rawAddFormats = rawAddFormats {
+            addFormats = Set(rawAddFormats.compactMap { intValue in
+                //needed because RTB.Format doesnt have an int value
+                switch intValue {
+                case 1:
+                    RTB.Format.banner
+                case 2:
+                    RTB.Format.mrec
+                case 3:
+                    RTB.Format.halfScreen
+                case 4:
+                    RTB.Format.leaderboard
+                case 5:
+                    RTB.Format.interstitialPortrait
+                case 6:
+                    RTB.Format.interstitialLandscape
+                case 7:
+                    RTB.Format.leaderboard
+                default:
+                    nil
+                }
+            })
+        }
+        return banner(size: adSize, addFormats: addFormats, adPosition: adPosition ?? .unknown, bidFloor: bidFloor, battr: battr ?? [])
+    }
+}
+
+
+protocol UnityRequestComponent {
+    @MainActor var requestComponent: any RequestComponent { get }
+}
+
+public struct VideoCreative: Codable, UnityRequestComponent, Sendable {
+    let adPosition: RTB.Position?
+    let bidFloor: Float?
+    let minDuration: Int?
+    let maxDuration: Int?
+    let width: Int?
+    let height: Int?
+    let rawPlacementType: Int?
+    let rawPlaybackMethod: [Int]?
+    
+    enum CodingKeys: String, CodingKey {
+        case adPosition, bidFloor, minDuration, maxDuration, width, height
+        case rawPlacementType = "placementType"
+        case rawPlaybackMethod = "playbackMethod"
+    }
+    
+    //this is needed because RTB.VideoPlacementType's declaration is overriding swift's normal decode methods
+    var placementType: RTB.VideoPlacementType? {
+        if let placement = rawPlacementType {
+            return RTB.VideoPlacementType(rawValue: placement)
+        }
+        return nil
+    }
+    
+    //this is needed because RTB.PlaybackMethod's declaration is overriding swift's normal decode methods
+    var playbackMethod: Set<RTB.PlaybackMethod>? {
+        guard let playbackMethod = rawPlaybackMethod else { return nil }
+        return Set(playbackMethod.compactMap { intValue in
+            RTB.PlaybackMethod(rawValue: intValue)
+        })
+    }
+    
+    var requestComponent: any NimbusKit.RequestComponent {
+        video(adPosition: adPosition ?? .unknown, bidFloor: bidFloor, minDuration: minDuration, maxDuration: maxDuration, width: width,
+              height: height, placementType: placementType, playbackMethod: playbackMethod ?? [])
+    }
+}
+
+public struct Env: Codable, UnityRequestComponent, Sendable {
+    let publisherKey: String
+    let apiKey: String
+    
+    var requestComponent: any NimbusKit.RequestComponent {
+        environment(publisherKey: publisherKey, apiKey: apiKey)
+    }
+}
+
+public struct Viewability: Codable, UnityRequestComponent, Sendable {
+    let omidPn: String
+    let omidPv: String
+    var requestComponent: any NimbusKit.RequestComponent {
+        viewability(omidpn: omidPn, omidpv: omidPv)
+    }
+}
+
+public struct PerRequestApp: Codable, UnityRequestComponent, Sendable {
+    let pageCat: Set<String>
+    let sectionCat: Set<String>
+    var requestComponent: any NimbusKit.RequestComponent {
+        app(pagecat: pageCat, sectioncat: sectionCat)
+    }
+}
+
+public struct Location: Codable, UnityRequestComponent, Sendable {
+    let latitude: Double
+    let longitude: Double
+    let accuracy: Int?
+    
+    private let rawlocationType: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude, longitude, accuracy
+        case rawlocationType = "locationType"
+    }
+    
+    //this is needed because RTB.Geo.LocationType's declaration is overriding swift's normal decode methods
+    var locationType: RTB.Geo.LocationType {
+        return RTB.Geo.LocationType(rawValue: rawlocationType) ?? RTB.Geo.LocationType.gps
+    }
+    
+    var requestComponent: any NimbusKit.RequestComponent {
+        location(latitude: latitude, longitude: longitude, type: locationType, accuracy: accuracy)
+    }
+}
+
+extension Extensions {
+    
+    struct AdMob: Codable {
+        let adUnitIds: [String?]?
+    }
+    
+    struct Aps: Codable {
+        let appKey: String?
+        let slotData: [ApsSlotData?]?
+    }
+    
+    struct ApsSlotData: Codable {
+        let slotId: String?
+        let adUnitType: APSAdUnitType?
+    }
+    
+    public enum APSAdUnitType: Int, Codable {
+        case display320X50
+        case display300X250
+        case display728X90
+        case interstitialDisplay
+        case interstitialVideo
+        case rewardedVideo
+    }
+    
+    struct InMobi: Codable {
+        let accountId: String?
+    }
+    
+    struct Meta: Codable {
+        let appId: String?
+        let forceTestAd: Bool
+    }
+    
+    struct Mintegral: Codable {
+        let appId: String?
+        let appKey: String?
+    }
+    
+    struct MobileFuse: Codable {
+    }
+    
+    struct Moloco: Codable {
+        let appKey: String?
+    }
+    
+    struct UnityAds: Codable {
+        let gameId: String?
+    }
+    
+    struct Vungle: Codable {
+        let appId: String?
+    }
+}
+
