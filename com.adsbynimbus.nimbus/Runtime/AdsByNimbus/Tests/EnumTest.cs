6ae70dd0e12823b91ced7eb49904373ac5896148
Jonathan Sligh <jonathan@nimbus.co>
2026-08-17 14:14:28 -0500

    Fixed enum test.

--- com.adsbynimbus.nimbus/Runtime/AdsByNimbus/Tests/EnumTest.cs
+++ com.adsbynimbus.nimbus/Runtime/AdsByNimbus/Tests/EnumTest.cs
@@ -5,8 +5,8 @@ namespace Tests {
 	public class EnumTest {
 		[TestCase(AdSize.banner, 320, 50,
 			TestName = "Check that Banner returns a 320x50 tuple")]
-		[TestCase(AdSize.mrec, 300, 600,
-			TestName = "Check that mrec returns a 300x600 tuple")]
+		[TestCase(AdSize.mrec, 300, 250,
+			TestName = "Check that mrec returns a 300x250 tuple")]
 		public void TestIabEnumSizes(AdSize size, int expectedWidth, int expectedHeight) {
 			var (width, height) = size.ToWidthAndHeight();
 			Assert.AreEqual(expectedWidth, width);
